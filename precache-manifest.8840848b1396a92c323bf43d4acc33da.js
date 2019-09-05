38d92563e8f4a77a964a4ee49964980ca65adb18
vimal Raveendran <vimalraveendra@gmail.com>
2019-09-05 21:32:56 +0200

    Updates

---
+++ precache-manifest.8840848b1396a92c323bf43d4acc33da.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad73c5fa26102eedf7b590870fcc0d0",
+    "url": "/redux-saga/index.html"
+  },
+  {
+    "revision": "d3fdc7ed8f6677fee0a9",
+    "url": "/redux-saga/static/css/main.6da4f4e7.chunk.css"
+  },
+  {
+    "revision": "53d894872ff8c8a3dbc7",
+    "url": "/redux-saga/static/js/2.f5748d0a.chunk.js"
+  },
+  {
+    "revision": "d3fdc7ed8f6677fee0a9",
+    "url": "/redux-saga/static/js/main.93fa633d.chunk.js"
+  },
+  {
+    "revision": "83f400b3e005a965a1c5",
+    "url": "/redux-saga/static/js/runtime~main.51cf09d8.js"
+  }
+]);
